8b28fafcbc5714f704830d25ba9c446fcef81b86
Kenny Guo <dev_kg@163.com>
2015-06-03 16:43:15 +0800

    finish user sign up

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+	def create
+		@user = User.new(user_params)
+
+		if @user.save then
+			flash[:success] = "Welcome buddy :)"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params[:user].permit(:name, :email, :password, :password_confirmation)
+	end
+
 end
